53005f7fcb09ad448a4927c5b314cbdb28e9c393
Christian Madufor <chrismadufor@gmail.com>
2021-07-29 22:22:45 +0100

    remodel of previous site

--- js/index.js
+++ js/index.js
@@ -3,5 +3,6 @@ const mobileNav = document.getElementById('mobile-nav');
 
 
 navBtn.addEventListener('click', () => {
+    document.body.classList.toggle('lock-scroll');
     mobileNav.classList.toggle ('open')
 })
