a82ec9d166fb804732c584aeeac948c35c17f70b
Christian Madufor <chrismadufor@gmail.com>
2021-07-30 06:38:07 +0100

    New updates

--- js/index.js
+++ js/index.js
@@ -2,7 +2,15 @@ const navBtn = document.getElementById('nav-btn');
 const mobileNav = document.getElementById('mobile-nav');
 
 
+
+
 navBtn.addEventListener('click', () => {
     document.body.classList.toggle('lock-scroll');
     mobileNav.classList.toggle ('open')
+    document.querySelector('main').addEventListener('click', () => {
+        if (mobileNav.classList.contains ('open')) {
+            mobileNav.classList.remove ('open')
+        }
+    })
 })
+
