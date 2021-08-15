edcf71060b6fe245464f2912f6568ded7eadc973
Christian Madufor <chrismadufor@gmail.com>
2021-08-15 22:39:18 +0100

    animated the mobile nav for better effect

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ const navBtn = document.getElementById('nav-btn');
 const mobileNav = document.getElementById('mobile-nav');
 
 
-
-
 navBtn.addEventListener('click', () => {
     document.body.classList.toggle('lock-scroll');
     mobileNav.classList.toggle ('open')
