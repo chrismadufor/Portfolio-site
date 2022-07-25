bedb3785b74c5200393426ef5dee53b81b1e22f4
chrismadufor <chrismadufor.com>
2022-07-25 23:23:00 +0100

    site update

---
+++ js/contact.js
@@ -0,0 +1,28 @@
+(function() {
+    emailjs.init('akQ6C7HPjPY2u6mX1');
+})()
+
+const fullName = document.querySelector('.name')
+const email = document.querySelector('.email')
+const message = document.querySelector('.message')
+const errorText = document.querySelector('.error-text')
+
+
+console.log(fullName)
+window.onload = function() {
+    document.getElementById('form-contact').addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (fullName.value === '' || email.value === '' || message.value === '') {
+        errorText.style.display = 'block'
+    } else {
+        errorText.style.display = 'none'
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('service_ib2a9er', 'template_2xz0pmb', this)
+            .then(function() {
+                alert('Thanks for reaching out. I will get back to you shortly');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+        }
+    });
+}
